34bf0f88b908542458bb507d2ba0179535c63bc9
clauviau <viauclaudia@gmail.com>
2026-04-25 18:18:55 -0400

    Refactor FinalProject.py for improved structure

--- FinalProject.py
+++ FinalProject.py
@@ -2,9 +2,7 @@ import pygame
 import random
 import time
 
-
 pygame.init()
-
 
 screen_width = 1200
 screen_height = 800
@@ -12,14 +10,8 @@ screen_height = 800
 screen = pygame.display.set_mode((screen_width, screen_height))
 clock = pygame.time.Clock()
 
-
-# desk and library images from https://opengameart.org/content/wooden-cupboard-shelf-pcdesk
-# wall image from https://opengameart.org/content/medieval-wall
-    #used GIMP to make wall_img.png bigger, by creating a canvas the size of the screen and ctr+c/ctr+v the initial image
-    #windows :  https://fr.freepik.com/photos-vecteurs-libre/fenetre-arche 
-
- #Level class understood from this video : https://www.youtube.com/watch?v=QZ6f9i_GE4s&list=PL117jAcXfXy5lV5A6gi1FEfjxSeBXD3Sz&index=2
- #structure of the cloud generation : https://www.youtube.com/watch?v=QZ6f9i_GE4s&list=PL117jAcXfXy5lV5A6gi1FEfjxSeBXD3Sz&index=4 
+def get_font(size):
+    return pygame.font.Font("FreeSansBold.otf", size)
 
 class Background():
     def __init__(self, screen):
@@ -29,12 +21,6 @@ class Background():
         
         self.wall = pygame.image.load('images/bigwall.png').convert_alpha() 
         self.wall = pygame.transform.scale(self.wall, (screen_width, screen_height))
-
-        #self.desk = pygame.image.load('images/woodDesk_img.png').convert_alpha()
-        #self.desk = pygame.transform.scale(self.desk, (180, 215))
-
-        self.door = pygame.image.load('images/closeddoor_img.png').convert_alpha()
-        self.door = pygame.transform.scale(self.door, (180, 300))
 
         self.cloud = pygame.image.load('images/clouds_img.png').convert_alpha()
         self.cloud = pygame.transform.scale(self.cloud, (110, 90))
@@ -46,7 +32,6 @@ class Background():
             speed = random.randint(2,4)
             self.clouds.append({"x": x, "y":y, "speed":speed})
      
-
     def update_cloud(self):
         for cloud in self.clouds:
             cloud["x"] += cloud["speed"]
@@ -63,15 +48,10 @@ class Background():
 
         self.screen.blit(self.wall, (0, 0))
         self.floor = pygame.draw.rect(self.screen,(75, 30, 0), (0, 680, 1200, 120))
-        #self.screen.blit(self.desk, (800, 500))
-        self.screen.blit(self.door, (100, 390))
  
     def run(self):
         self.update_cloud()
         self.draw()
-
-def get_font(size):
-    return pygame.font.Font("FreeSansBold.otf", size)
 
 class Buttons():
     def __init__(self, image, x_pos, y_pos, size_hover, hover_pos):
@@ -97,11 +77,11 @@ class Buttons():
     def checkForInput_desk(self, position):
         if position[0] in range(self.rect.left, self.rect.right) and position[1] in range(self.rect.top, self.rect.bottom):
             return True
-            #self.text = get_font(50).render(self.text, True, 'black')
-            #self.text_rect = self.text.get_rect(center=(600, 400))
-            #screen.blit(self.text, self.text_rect)
-            
-            
+    def checkForInput_door(self, position):
+        if position[0] in range(self.rect.left, self.rect.right) and position[1] in range(self.rect.top, self.rect.bottom):
+            return True
+
+                    
     def change_color(self, position):
         if position[0] in range(self.rect.left, self.rect.right) and position[1] in range(self.rect.top, self.rect.bottom):
             screen.blit(self.hover_image, self.hover_image_rect)
@@ -111,22 +91,35 @@ class Buttons():
         self.update()
 
 
-
 button_library = pygame.image.load('images/library_img.png').convert_alpha()
 button_library = pygame.transform.scale(button_library, (180, 250))
 
 button_desk = pygame.image.load('images/woodDesk_img.png').convert_alpha()
 button_desk = pygame.transform.scale(button_desk, (180, 215))
 
+button_door = pygame.image.load('images/closeddoor_img.png').convert_alpha()
+button_door = pygame.transform.scale(button_door, (180, 300))
 
 background = Background(screen)
 buttons_library = Buttons(button_library, 550, 440, (195, 260),(542, 435))
 buttons_desk = Buttons(button_desk,800, 500, (190, 225), (795, 495))
+buttons_door = Buttons(button_door, 100, 390, (190, 315), (95, 380))
 
-#self.blit(self.desk, (800, 500))
-#self.screen.blit(self.library, (550, 440))
 
-message = ""
+show_message = False
+message_start_time = 0
+
+def not_available():
+    current_time = pygame.time.get_ticks()
+    message = "Not available at this time!"
+
+    if show_message and current_time - message_start_time < 4000:
+            text_surface = get_font(50).render(message, True, "red")
+            text_rect = text_surface.get_rect(center=(600, 400))
+            screen.blit(text_surface, text_rect)
+ 
+    
+
 running = True
 while running:
     mouse_pos = pygame.mouse.get_pos()
@@ -139,25 +132,41 @@ while running:
         if event.type == pygame.MOUSEBUTTONDOWN:
             buttons_library.checkForInput_library(mouse_pos)
             if buttons_desk.checkForInput_desk(mouse_pos):
-                message = "Not available at this time!"
-            
+                show_message = True
+                message_start_time = pygame.time.get_ticks()
+            if buttons_door.checkForInput_door(mouse_pos):
+                show_message = True
+                message_start_time = pygame.time.get_ticks()
+        
     background.run()
     buttons_library.run()
     buttons_desk.run()
+    buttons_door.run()
     buttons_library.change_color(mouse_pos)
     buttons_desk.change_color(mouse_pos)
+    buttons_door.change_color(mouse_pos)
 
-    if message != "":
-        message_time = pygame.time.get_ticks()
-        if message_time < 4000:
-            text_surface = get_font(50).render(message, True, "red")
-            text_rect = text_surface.get_rect(center=(600, 400))
-            screen.blit(text_surface, text_rect)
-        else:
-            message = ""
+    not_available()
+    current_time = pygame.time.get_ticks()
+    if show_message and current_time - message_start_time >= 2000:
+        show_message = False
 
-
+    
     pygame.display.flip()
     clock.tick(60)   
 
 pygame.quit()
+
+
+
+
+
+
+
+# desk and library images from https://opengameart.org/content/wooden-cupboard-shelf-pcdesk
+# wall image from https://opengameart.org/content/medieval-wall
+    #used GIMP to make wall_img.png bigger, by creating a canvas the size of the screen and ctr+c/ctr+v the initial image
+    #windows :  https://fr.freepik.com/photos-vecteurs-libre/fenetre-arche 
+
+ #Level class understood from this video : https://www.youtube.com/watch?v=QZ6f9i_GE4s&list=PL117jAcXfXy5lV5A6gi1FEfjxSeBXD3Sz&index=2
+ #structure of the cloud generation : https://www.youtube.com/watch?v=QZ6f9i_GE4s&list=PL117jAcXfXy5lV5A6gi1FEfjxSeBXD3Sz&index=4 
